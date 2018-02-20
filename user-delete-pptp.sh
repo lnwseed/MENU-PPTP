efb61fbae72da9eb93ed7549b2793cd7c21c1d73
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2018-02-20 14:10:34 +0700

    Update user-delete-pptp.sh

--- user-delete-pptp.sh
+++ user-delete-pptp.sh
@@ -6,22 +6,22 @@ red='\e[1;31m'
 green='\e[0;32m'
 blue='\e[1;34m'
 NC='\e[0m'
-echo "Connecting to VPS-murah.net..."
+echo "Connecting to เฮียเบิร์ด.com..."
 sleep 0.2
 echo "Checking Permision..."
 sleep 0.3
 CEK=`curl -s http://api.vps-murah.net/api/checker.php?mode=trial`;
-if [ "$CEK" != "MEMBER" ]; then
+if [ "$CEK" != "on" ]; then
 		echo -e "${red}Permission Denied!${NC}";
         echo $CEK;
         exit 0;
 else
-echo -e "${green}Permission Accepted...${NC}"
+echo -e "${green}ได้รับอนุญาตแล้ว...${NC}"
 sleep 1
 clear
 fi
 
-read -p "Masukkan Username yang ingin anda hapus: " username
+read -p "ป้อนชื่อผู้ใช้ที่คุณต้องการลบ : " username
 grep -E "^$username" /etc/ppp/chap-secrets >/dev/null
 if [ $? -eq 0 ]; then
 # proses mengganti passwordnya
@@ -31,14 +31,14 @@ sed -i $username2 /var/lib/premium-script/data-user-pptp
 sed -i '/^$/d' /etc/ppp/chap-secrets
 sed -i '/^$/d' /var/lib/premium-script/data-user-pptp
 clear
-  echo "Script by vps-murah.net"
-  echo "Terimakasih sudah berlangganan di vps-murah.net"
+  echo "Script by เฮียเบิร์ด.com"
+  echo "Thank you for subscribing at เฮียเบิร์ด.com"
   echo " "
   echo "-----------------------------------------------"
-  echo -e "Username ${blue}$username${NC} Sudah berhasil di ${red}HAPUS${NC}."
-  echo -e "Akses Login untuk username ${blue}$username${NC} sudah dihapus"
+  echo -e "Username ${blue}$username${NC} It's been successful at ${red}HAPUS${NC}."
+  echo -e "Login Access for username ${blue}$username${NC} ลบแล้ว"
   echo "-----------------------------------------------"
 else
-echo "Username tidak ditemukan di server anda"
+echo "ไม่พบชื่อผู้ใช้บนเซิร์ฟเวอร์ของคุณ"
     exit 1
 fi
